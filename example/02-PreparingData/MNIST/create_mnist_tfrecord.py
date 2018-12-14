4eb45d8c5727062ee0629d0e69b332284375633d
ben <bbclr20@gmail.com>
2018-12-14 16:01:47 +0800

    Modify MNIST tfrecords example

--- example/02-PreparingData/MNIST/create_mnist_tfrecord.py
+++ example/02-PreparingData/MNIST/create_mnist_tfrecord.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
+import os
 
 def _int64_feature (value): 
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
@@ -8,7 +9,7 @@ def _int64_feature (value):
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-MNIST_DIR = "../../data/mnist"
+MNIST_DIR = "../../../data/mnist"
 
 mnist = input_data.read_data_sets(MNIST_DIR, one_hot=False)
 images = mnist.train.images
@@ -17,18 +18,32 @@ labels = mnist.train.labels
 num_examples, pixels = images.shape
 print("MNIST num_examples: {}, pixels: {}".format(num_examples, pixels))
 
-filename = "./mnist.tfrecords"
-with tf.python_io.TFRecordWriter(filename) as writer:
-    for idx in range(num_examples):
-        image_raw = images[idx].tostring() # images[idx].dtype is float32
-        example = tf.train.Example(
-            features = tf.train.Features (
-                feature = {
-                    "pixels": _int64_feature(pixels),
-                    "label": _int64_feature(labels[idx]),
-                    "image_raw": _bytes_feature(image_raw),
-                }
+DATA_DIR = "./data"
+if tf.gfile.IsDirectory(DATA_DIR):
+    tf.gfile.DeleteRecursively(DATA_DIR)
+tf.gfile.MakeDirs(DATA_DIR)
+
+# seperate the data into n tfrecords files
+num_shards = 5
+instances_per_shard = int(num_examples/num_shards)
+
+for i in range(num_shards):
+    filename = "mnist.tfrecords-{:03d}-{:03d}".format(i, num_shards)
+    filename = os.path.join(DATA_DIR, filename)
+
+    with tf.python_io.TFRecordWriter(filename) as writer:
+        for j in range(instances_per_shard):
+        # for idx in range(num_examples):
+            idx = i * instances_per_shard + j
+            image_raw = images[idx].tostring() # images[idx].dtype is float32
+            example = tf.train.Example(
+                features = tf.train.Features (
+                    feature = {
+                        "pixels": _int64_feature(pixels),
+                        "label": _int64_feature(labels[idx]),
+                        "image_raw": _bytes_feature(image_raw),
+                    }
+                )
             )
-        )
-        writer.write(example.SerializeToString())
+            writer.write(example.SerializeToString())
 print("Done!!")
--- example/02-PreparingData/MNIST/read_mnist_tfrecord.py
+++ example/02-PreparingData/MNIST/read_mnist_tfrecord.py
@@ -1,10 +1,16 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from glob import glob
+import os
 
 reader = tf.TFRecordReader()
 
-TFRECORD_FILE = "./mnist.tfrecords"
-filename_queue = tf.train.string_input_producer([TFRECORD_FILE])
+DATA_DIR = "./data"
+FILE_PATTERN = "mnist.tfrecords-*-*"
+files = glob(os.path.join(DATA_DIR, FILE_PATTERN))
+print(files)
+
+filename_queue = tf.train.string_input_producer(files)
 
 _, serialized_example = reader.read(filename_queue)
 
@@ -18,22 +24,34 @@ features = tf.parse_single_example(
 )
 
 pixels = tf.cast(features["pixels"], tf.int32)
-labels = tf.cast(features["label"], tf.int32)
-images = tf.decode_raw(features["image_raw"], tf.float32)
-images = tf.reshape(images, [-1, 28, 28])
+label = tf.cast(features["label"], tf.int32)
+image = tf.decode_raw(features["image_raw"], tf.float32)
+image = tf.reshape(image, [28, 28])
+
+batch_size = 25
+capacity = 10 * batch_size
+min_after_dequeue = 8 * batch_size
+
+iamge_batch, label_batch = tf.train.shuffle_batch(
+    [image, label], batch_size=batch_size, 
+    capacity=capacity, min_after_dequeue=min_after_dequeue)
+
 
 with tf.Session() as sess:
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-    # read a record
-    pixel, label, image = sess.run([pixels, labels, images])
+    # read a batch
+    images, labels = sess.run([iamge_batch, label_batch])
 
     coord.request_stop()
     coord.join(threads)
     
-    plt.figure("MNIST")
-    plt.title(label)
-    plt.imshow(image[0])
-    plt.axis("off")
+    for idx, (img, lab) in enumerate(zip(images, labels)):
+        plt.figure("MNIST")
+        plt.subplot(5, 5, idx+1)
+        plt.title(lab)
+        plt.imshow(img)
+        plt.axis("off")
     plt.show()
+    
